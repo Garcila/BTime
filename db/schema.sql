964a3531630e4cad5547637bc0871c57b37dfe01
garcila <germanarcila@gmail.com>
2019-03-04 06:27:21 -0500

    connected to Heroku and populated DB

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,13 @@
-DROP DATABASE burgers_db;
-CREATE DATABASE burgers_db;
-USE burgers_db;
+-- DROP DATABASE burgers_db;
+-- CREATE DATABASE burgers_db;
+-- USE burgers_db;
+USE ncm25w3t5c9wapzq;
 
 CREATE TABLE burgers (
   id INT NOT NULL AUTO_INCREMENT,
   burger_name VARCHAR(30) NOT NULL,
   devoured BOOLEAN DEFAULT false,
+  createdAt TIMESTAMP NOT NULL,
   PRIMARY KEY (id)
 )
 
